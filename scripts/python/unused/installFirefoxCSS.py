9837ab6a92d4f82f284a13b7fb582bbd064ee531
hakan-demirli <emre.demirli@metu.edu.tr>
2023-08-30 00:08:19 +0300

    add user.js to firefox css
    remove discord from hyprland

--- scripts/python/unused/installFirefoxCSS.py
+++ scripts/python/unused/installFirefoxCSS.py
@@ -24,6 +24,18 @@ def cloneRepoToChromeFolder():
         git_command = ["git", "clone", mylib.FIREFOX_CSS_URL, chrome_folder_path]
         subprocess.run(git_command, check=True)
         print("Repository cloned successfully.")
+
+        # Move user.js to the parent folder
+        userjs_path = os.path.join(chrome_folder_path, "user.js")
+        parent_folder_path = os.path.dirname(chrome_folder_path)
+        new_userjs_path = os.path.join(parent_folder_path, "user.js")
+
+        if os.path.exists(userjs_path):
+            shutil.move(userjs_path, new_userjs_path)
+            print("user.js moved successfully.")
+        else:
+            print("user.js not found in the chrome folder.")
+
     else:
         print("Unable to find the Firefox profile folder.")
 
