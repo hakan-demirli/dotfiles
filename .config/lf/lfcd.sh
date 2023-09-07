d20b1f247c9b508a73e8c9c606bd96dc890f24b6
hakan-demirli <emre.demirli@metu.edu.tr>
2023-09-07 17:26:35 +0300

    add lf to hyprland

--- .config/lf/lfcd.sh
+++ .config/lf/lfcd.sh
@@ -16,10 +16,9 @@
 
 lfcd () {
     tmp="$(mktemp)"
-    # `command` is needed in case `lfcd` is aliased to `lf`
-    command lf -last-dir-path="$tmp" "$@"
+    lf -last-dir-path="$tmp" "$@"
     if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
+        dir="$(command cat "$tmp")"
         rm -f "$tmp"
         if [ -d "$dir" ]; then
             if [ "$dir" != "$(pwd)" ]; then
