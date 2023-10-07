45352cc083f67f4e8540bc52344f21ab3c5d3830
hakan-demirli <emre.demirli@metu.edu.tr>
2023-10-07 21:22:43 +0300

    add missing dependency ripgrep
    change keyboard layers

--- scripts/lin/ib.sh
+++ scripts/lin/ib.sh
@@ -208,6 +208,7 @@ function i_helix() {
     sudo pacman -S --noconfirm --needed bat # live-grep script
     sudo pacman -S --noconfirm --needed tmux
     sudo pacman -S --noconfirm --needed fzf
+    sudo pacman -S --noconfirm --needed ripgrep
     # LSPs and Formatters
     sudo pacman -S --noconfirm --needed prettier     # markdown etc.
     sudo pacman -S --noconfirm --needed pyright      # python
