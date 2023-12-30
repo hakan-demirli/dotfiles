80f92767dc1c74d0a004cf9714a66a843af8816f
hakan-demirli <emre.demirli@metu.edu.tr>
2023-12-30 11:28:51 +0300

    fix: keyboard layout
    fix: battery monitor missing dependency
    
    feat: add auto refresh rate changer

---
+++ programs/auto_refresh.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}:
+pkgs.stdenv.mkDerivation {
+  name = "auto_refresh";
+
+  nativeBuildInputs = with pkgs; [
+    wrapGAppsHook
+    gobject-introspection
+    libgudev
+    libnotify
+  ];
+  propagatedBuildInputs = [
+    (pkgs.python3.withPackages (pythonPackages:
+      with pythonPackages; [
+        pygobject3
+      ]))
+  ];
+  dontUnpack = true;
+  installPhase = ''
+    install -Dm755 ${../scripts/python/auto_refresh.py} $out/bin/auto_refresh;
+  '';
+}
---
+++ programs/xdg.nix
@@ -0,0 +1,91 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  xdg = let
+    mutable_configs = ["hypr"];
+    immutable_configs = [
+      "bat"
+      "gnome3-keybind-backup"
+      "kitty"
+      "nix"
+      "sioyek"
+      "udiskie"
+      "wofi"
+      "btop"
+      "gnome-extensions"
+      "lf"
+      "npm"
+      "swaync"
+      "vim"
+      "xremap"
+      "cargo"
+      "helix"
+      "mimeapps.list"
+      "nvim"
+      "tmux"
+      "wavemon"
+      "yazi"
+      "firefoxcss"
+      "input-remapper-2"
+      "mpv"
+      "nwg"
+      "tmuxp"
+      "waybar"
+      "zathura"
+      "git"
+      "mylib"
+      "qutebrowser"
+      "wgetrc"
+      "task"
+    ];
+
+    immutable_data = ["applications" "fonts"];
+    mutable_data = ["task"];
+
+    makeMutable = path: file: {
+      target = file;
+      source = config.lib.file.mkOutOfStoreSymlink "/home/emre/dotfiles/${path}/${file}";
+      recursive = true;
+    };
+
+    makeImmutable = path: file: {
+      target = file;
+      source = ../${path}/${file};
+      recursive = true;
+    };
+
+    mutableConfigFiles = builtins.listToAttrs (map (file: {
+        name = file;
+        value = makeMutable ".config" file;
+      })
+      mutable_configs);
+
+    immutableConfigFiles = builtins.listToAttrs (map (file: {
+        name = file;
+        value = makeImmutable ".config" file;
+      })
+      immutable_configs);
+
+    mutableDataFiles = builtins.listToAttrs (map (file: {
+        name = file;
+        value = makeMutable ".local/share" file;
+      })
+      mutable_data);
+
+    immutableDataFiles = builtins.listToAttrs (map (file: {
+        name = file;
+        value = makeImmutable ".local/share" file;
+      })
+      immutable_data);
+  in {
+    configFile = mutableConfigFiles // immutableConfigFiles;
+    dataFile = mutableDataFiles // immutableDataFiles;
+  };
+  home.file.".local/bin" = {
+    source = ../.local/bin;
+    recursive = true;
+    executable = true;
+  };
+}
