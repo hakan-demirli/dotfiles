be7c62af2ec2ac85bc3243708a87ed05cfa662ae
hakan-demirli <emre.demirli@metu.edu.tr>
2023-12-30 12:11:17 +0300

    fix: gtk applet

---
+++ scripts/python/gtk_applet/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Python flake env";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        shellHook = ''
+          echo "Sup."
+        '';
+        packages = with pkgs; [
+          python3
+          python3Packages.pygobject3
+          python3Packages.requests
+
+          wrapGAppsHook
+          gobject-introspection
+          libappindicator
+        ];
+      };
+    });
+}
