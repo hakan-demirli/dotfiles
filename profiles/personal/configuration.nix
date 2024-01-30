f333c37b4a9a3702d1b77dfaaace8d007b5053b2
hakan-demirli <emre.demirli@metu.edu.tr>
2024-01-30 18:53:51 +0300

    feat: add trusted caches

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -29,6 +29,15 @@
     };
 
     settings = {
+      substituters = [
+        "https://cuda-maintainers.cachix.org"
+        "https://openlane.cachix.org"
+      ];
+      # trusted-public-keys = [
+      #   "openlane.cachix.org-1:qqdwh+QMNGmZAuyeQJTH9ErW57OWSvdtuwfBKdS254E="
+      #   "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      # ];
+
       max-jobs = systemSettings.threads;
       cores = systemSettings.threads;
       experimental-features = "nix-command flakes";
