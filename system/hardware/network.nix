4c18a7c66e5bed626ef1c6c6d9b4aa3aa6f9d3bd
hakan-demirli <emre.demirli@metu.edu.tr>
2024-02-28 13:22:13 +0300

    feat: enable ipv4 forwarding

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -10,13 +10,17 @@
   networking.extraHosts = ''
     0.0.0.0  9gag.com
     0.0.0.0  www.9gag.com
-    0.0.0.0  reddit.com
-    0.0.0.0  www.reddit.com
+    # 0.0.0.0  reddit.com
+    # 0.0.0.0  www.reddit.com
   '';
 
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    # "net.ipv6.conf.all.forwarding" = 1;
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 }
