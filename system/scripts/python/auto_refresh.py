83bd7d912e8d7efb182ed4b0c8b2c85def120ace
hakan-demirli <ehdemirli@gmail.com>
2024-04-02 21:00:55 +0300

    fix: change refresh rate at boot

--- system/scripts/python/auto_refresh.py
+++ system/scripts/python/auto_refresh.py
@@ -14,6 +14,13 @@ def sed(regex: str, path: str):
     subprocess.run(["sed", "-i", regex, path])
 
 
+def check_initial_power_status(client):
+    devices = client.query_by_subsystem("power_supply")
+    for device in devices:
+        if device.get_property("POWER_SUPPLY_ONLINE") == "1":
+            ac_event_handler(client, "change", device, None)
+
+
 def ac_event_handler(client, action, device, user_data):
     CONFIG_FILE_PATH = "~/.config/hypr/monitors.conf"  # ABS_PATH: fix pls
     CONFIG_FILE_PATH = os.path.expanduser(CONFIG_FILE_PATH)
@@ -22,22 +29,15 @@ def ac_event_handler(client, action, device, user_data):
     TO_MAX_REGEX = f"/eDP-1/s/@{MIN_REFRESH_RATE}/@{MAX_REFRESH_RATE}/g"
     TO_MIN_REGEX = f"/eDP-1/s/@{MAX_REFRESH_RATE}/@{MIN_REFRESH_RATE}/g"
 
-    # property_keys = device.get_property_keys()
-    # # Print all properties and their values
-    # for key in property_keys:
-    #     value = device.get_property(key)
-    #     print(f"{key}: {value}")
     if action == "change":
         if device.get_property("SUBSYSTEM") == "power_supply":
             online = device.get_property("POWER_SUPPLY_ONLINE")
             if online == "1":
-                # print("AC Adapter Plugged In")
                 with open(CONFIG_FILE_PATH, "r") as file:
                     file_contents = file.read()
                 if f"@{MAX_REFRESH_RATE}" not in file_contents:
                     sed(TO_MAX_REGEX, CONFIG_FILE_PATH)
             elif online == "0":
-                # print("AC Adapter Unplugged")
                 sed(TO_MIN_REGEX, CONFIG_FILE_PATH)
         if device.get_property("POWER_SUPPLY_CAPACITY_LEVEL") == "critical":
             subprocess.run(
@@ -54,6 +54,9 @@ def main():
     client = GUdev.Client(subsystems=["power_supply"])
     client.connect("uevent", ac_event_handler, None)
 
+    # Check the initial power status
+    check_initial_power_status(client)
+
     # Create a GLib MainLoop to listen for events
     loop = GLib.MainLoop()
     loop.run()
@@ -62,6 +65,6 @@ def main():
 if __name__ == "__main__":
     """
     Monitors power supply events and automatically changes monitor refresh rate.
-    Only Hyprland Window Manager is supported. 
+    Only Hyprland Window Manager is supported.
     """
     main()
