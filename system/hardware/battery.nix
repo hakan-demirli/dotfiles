af39b4694790e406cecaef33052448725510d313
hakan-demirli <ehdemirli@gmail.com>
2024-08-07 23:06:23 +0300

    fix: disable tlp, too much lag

--- system/hardware/battery.nix
+++ system/hardware/battery.nix
@@ -1,10 +1,10 @@
 { ... }:
 {
   # $ nix search wget
-  powerManagement = {
-    enable = true;
-    cpuFreqGovernor = "ondemand";
-  };
+  # powerManagement = {
+  #   enable = true;
+  #   cpuFreqGovernor = "ondemand";
+  # };
 
   services.tlp = {
     enable = true;
@@ -12,13 +12,15 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MAX_PERF_ON_BAT = 70;
+
+      RUNTIME_PM_DRIVER_BLACKLIST = "nouveau nvidia"; # TODO: check if this prevents laggy mouse
     };
   };
 }
