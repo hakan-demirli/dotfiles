61eff12ad040bed0677ca2e2323fe15fc885da7a
hakan-demirli <ehdemirli@gmail.com>
2024-11-04 10:31:26 +0300

    fix: disable alt modifier on func layer

--- .local/share/qmk/tbkmini7x3/keymaps/default/keymap.c
+++ .local/share/qmk/tbkmini7x3/keymaps/default/keymap.c
@@ -26,57 +26,65 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY_LEFT] = LAYOUT(
         // Left
            KC_TAB,        KC_Q,       KC_W,       KC_E,        KC_R,          KC_T,         KC_LCTL,
-           KC_ESC,        KC_A,       KC_S,       KC_D,        KC_F,          KC_G,         MO_NO_MOD,
+           KC_ESC,        KC_A,       KC_S,       KC_D,        KC_F,          KC_G,         MO(FUN_LAYER),  // Changed to MO
            KC_LSFT,       KC_Z,       KC_X,       KC_C,        KC_V,          KC_B,         KC_SPC,
         // Right
            KC_RCTL,       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,      KC_LBRC,
-           _______,       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,      KC_QUOTE,
+           XXXXXXX,       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,      KC_QUOTE,
            KC_RGUI,       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,      KC_RSFT
         ),
     [QWERTY_RIGHT] = LAYOUT(
         // Left
            KC_TAB,        XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,       XXXXXXX,      KC_LCTL,
-           KC_ESC,        XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,       XXXXXXX,      _______,
+           KC_ESC,        XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,       XXXXXXX,      XXXXXXX,
            KC_LSFT,       XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,       XXXXXXX,      KC_SPC,
         // Right
            KC_RCTL,       KC_Y,       KC_U,       KC_I,        KC_O,          KC_P,         KC_LBRC,
-           MO_NO_MOD,     KC_H,       KC_J,       KC_K,        KC_L,          KC_SCLN,      KC_QUOTE,
+           MO(FUN_LAYER), KC_H,       KC_J,       KC_K,        KC_L,          KC_SCLN,      KC_QUOTE,      // Changed to MO
            KC_RGUI,       KC_N,       KC_M,       KC_COMM,     KC_DOT,        KC_SLSH,      KC_RSFT
         ),
     [FUN_LAYER] = LAYOUT(
         // Left
            KC_F1,         KC_F2,      KC_F3,      KC_F4,       KC_F5,         KC_F6,        XXXXXXX,
-           QK_BOOT,       KC_LGUI,    XXXXXXX,    KC_MS_BTN2,  KC_MS_BTN1,    XXXXXXX,      _______,
-           XXXXXXX,       XXXXXXX,    XXXXXXX,    KC_MS_WH_UP, KC_MS_WH_DOWN, XXXXXXX,      XXXXXXX,
+           KC_ESC,        XXXXXXX,    XXXXXXX,    KC_MS_BTN2,  KC_MS_BTN1,    XXXXXXX,      XXXXXXX,
+           XXXXXXX,       QK_BOOT,    XXXXXXX,    KC_MS_WH_UP, KC_MS_WH_DOWN, XXXXXXX,      XXXXXXX,
         // Right
            XXXXXXX,       KC_F7,      KC_F8,      KC_F9,       KC_F10,        KC_F11,       KC_F12,
-           _______,       KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP,    KC_MS_RIGHT,   XXXXXXX,      XXXXXXX,
+           XXXXXXX,       KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP,    KC_MS_RIGHT,   XXXXXXX,      XXXXXXX,
            XXXXXXX,       XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,       XXXXXXX,      XXXXXXX
         ),
     // clang-format on
 };
 
+// Function to handle custom layer state changes
+layer_state_t layer_state_set_user(layer_state_t state)
+{
+    // Clear mods when entering FUN_LAYER
+    if (layer_state_cmp(state, FUN_LAYER)) {
+        clear_mods();
+        clear_weak_mods();
+        clear_oneshot_mods();
+        unregister_mods(MOD_MASK_SHIFT | MOD_MASK_CTRL | MOD_MASK_ALT | MOD_MASK_GUI);
+    }
+    return state;
+}
+
+uint8_t mod_state;
 bool process_record_user(uint16_t keycode, keyrecord_t* record)
 {
-    static bool no_mod_active = false;
+    // Store mod state when pressing any key
+    if (record->event.pressed) {
+        mod_state = get_mods();
+    }
 
     switch (keycode) {
-    case MO_NO_MOD:
-        if (record->event.pressed) {
-            no_mod_active = true;
-            clear_mods();
-            clear_oneshot_mods();
-            layer_on(FUN_LAYER);
-        } else {
-            no_mod_active = false;
-            layer_off(FUN_LAYER);
-        }
-        return false;
     case KC_MS_WH_UP:
     case KC_MS_WH_DOWN:
-        if (no_mod_active) {
+        if (IS_LAYER_ON(FUN_LAYER)) {
             clear_mods();
+            clear_weak_mods();
             clear_oneshot_mods();
+            unregister_mods(MOD_MASK_SHIFT | MOD_MASK_CTRL | MOD_MASK_ALT | MOD_MASK_GUI);
         }
         return true;
     }
