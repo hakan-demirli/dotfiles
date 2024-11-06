aa117249fc34038300da571f3da50f82ca8591bf
hakan-demirli <ehdemirli@gmail.com>
2024-11-06 21:34:28 +0300

    chore: bump lock file

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -330,7 +330,7 @@
     (pkgs.callPackage ../../system/app/mitype.nix { })
     (pkgs.callPackage ../../system/app/gen_typing_test.nix { })
     (pkgs.callPackage ../../system/app/print_weather.nix { })
-    (pkgs.callPackage ../../system/app/blender.nix { })
+    # (pkgs.callPackage ../../system/app/blender.nix { })
 
     (pkgs.callPackage ../../system/app/veridian.nix { })
     # (pkgs.callPackage ../../system/app/veridian.nix { withSlang = true; })
