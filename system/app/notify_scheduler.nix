a781c0315abba89e4f6f24eae3d357479a8a8354
hakan-demirli <ehdemirli@gmail.com>
2024-12-22 21:38:57 +0300

    feat: add notify_scheduler

---
+++ system/app/notify_scheduler.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+pkgs.stdenv.mkDerivation {
+  name = "notify_scheduler";
+
+  propagatedBuildInputs = [
+    (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [ requests ]))
+  ];
+  dontUnpack = true;
+  installPhase = ''
+    install -Dm755 ${../scripts/python/notify_scheduler.py} $out/bin/notify_scheduler;
+  '';
+}
