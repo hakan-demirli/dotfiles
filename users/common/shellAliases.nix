ce3df74457183282f10410c50f29fc46679a6b62
hakan-demirli <ehdemirli@gmail.com>
2025-02-16 01:40:16 +0300

    feat: refactor home.nix

---
+++ users/common/shellAliases.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+{
+
+  home.shellAliases = {
+    ":q" = "exit";
+    "q:" = "exit";
+    ":wq" = "exit";
+    "hx." = "helix .";
+    ".." = "cd ..";
+    "c" = "clear";
+    "cd.." = "cd ..";
+    helix = "hx";
+    lf = ''echo "Did you mean f?"''; # muscle memory
+    # f = "lf_cd";
+    f = "yazi_cd";
+    ff = "yazi_cd";
+    cdf = ''cd "$(find . -type d | fzf)"'';
+    da = "direnv allow";
+    # tt = ''tt --window_state=list'';
+    # tl = ''task list'';
+
+    git = "git_cached";
+    "ga." = "git add .";
+    ga = "git add";
+    gd = "git -c diff.external=difft diff";
+    gdc = "git -c diff.external=difft diff --cached";
+    gp = "git push";
+    gpf = "git push --force";
+    gr = "git restore";
+    "gr." = "git restore .";
+    grs = "git restore --staged";
+    gs = "git status";
+    gb = "fzf_git_branches";
+    gc = "git commit";
+    gcm = "git commit -m ";
+    gca = "git commit --amend";
+    gl = "fzf_git_commits";
+
+    # ascp = "asusctl profile -p";
+    # ascl = "asusctl profile -l";
+    # ascsp = "asusctl profile -P Performance";
+    # ascsb = "asusctl profile -P Balanced";
+    # ascsq = "asusctl profile -P Quiet";
+    yarn = ''yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'';
+
+    tmux = "tmux -f ~/.config/tmux/tmux.conf";
+    txa = ''tmux attach-session -t $(tmux list-sessions -F "#{session_name}" | head -n 1)'';
+    txls = "tmux list-sessions";
+    txks = "tmux kill-session -t ";
+    txn = "tmux new-session -s";
+    txs = "tmux switch-client -n";
+    txkw = "tmux kill-window -t ";
+    txlw = "tmux list-windows";
+    # txh = ''tmux new-session -s "$(basename "$(pwd)")_$(echo -n "$(pwd)" | md5sum | cut -d " " -f 1)" "hx ."'';
+
+    wget = ''wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'';
+    arduino-cli = "arduino-cli --config-file $XDG_CONFIG_HOME/arduino15/arduino-cli.yaml";
+
+    # llama = ''nix run git+https://github.com/nixified-ai/flake.git#textgen-nvidia'';
+
+    fan-turbo = ''cd /sys/devices/platform/asus-nb-wmi; sudo sh -c "echo 1 >>  fan_boost_mode"; sudo sh -c "echo 1 >> throttle_thermal_policy"; source ~/.bashrc; cd ~;'';
+    fan-performance = ''cd /sys/devices/platform/asus-nb-wmi; sudo sh -c "echo 0 >>  fan_boost_mode"; sudo sh -c "echo 0 >> throttle_thermal_policy"; source ~/.bashrc; cd ~;'';
+    fan-silent = ''cd /sys/devices/platform/asus-nb-wmi; sudo sh -c "echo 2 >>  fan_boost_mode"; sudo sh -c "echo 2 >> throttle_thermal_policy"; source ~/.bashrc; cd ~;'';
+  };
+}
--- users/emre-server/home.nix
+++ users/emre-server/home.nix
@@ -9,27 +9,14 @@ let
 in
 {
   imports = [
-    (import ../../pkgs/derivations/firefox.nix {
-      inherit username;
-    })
     (import ../../pkgs/derivations/xdg.nix {
       inherit pkgs inputs config;
       gdriveDir = /home/${username}/Desktop/gdrive;
       dotfilesDir = /home/${username}/Desktop/dotfiles;
     })
-    ../../pkgs/derivations/thunderbird.nix
-    ../../pkgs/derivations/low_battery_notify.nix
-
     ../common/sessionVariables.nix
     ../common/shellAliases.nix
   ];
-
-  dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = [ "qemu:///system" ];
-      uris = [ "qemu:///system" ];
-    };
-  };
 
   targets.genericLinux.enable = true;
 
@@ -135,20 +122,6 @@ in
 
   };
   home.packages = with pkgs; [
-    # awatcher
-    # aw-watcher-afk
-    # aw-watcher-window
-    # aw-server-rust
-    # (pkgs.callPackage ../../system/app/aw-manager.nix { })
-    # (pkgs.callPackage ../../system/app/aw-watchers-mine.nix { })
-
-    # (pkgs.callPackage ../../system/app/prometheus-exporters.nix { })
-    #
-    (pkgs.callPackage ../../pkgs/derivations/quantifyself.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/quantifyself-webui.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/html-preview-lsp.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/html-preview-server.nix { })
-
     bandwhich # network monitoring TUI
     cpufrequtils
     hypridle
@@ -168,8 +141,6 @@ in
     wl-clip-persist
     pulseaudio
     tree
-    anki-bin
-    piper-tts
 
     python3
 
@@ -193,12 +164,8 @@ in
 
     playerctl
     swww # for update_wp
-    activate-linux # for update_wp
     mpv
-    # (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-    # nerd-fonts.jetbrains-mono
     ripgrep
-    # ripdrag # started to fail when nvidia gpu is enabled
     xdragon
     tmux
     ffmpeg-full
@@ -212,26 +179,13 @@ in
     unar
     zip
     ghostscript
-    drawio
 
-    rclone
-    # citrix_workspace
-    # helix
-    # helix-gpt
-    # (koboldcpp.override { cublasSupport = true; })
-    # (pkgs.llama-cpp.override { cudaSupport = true; })
-    ollama-cuda
-    lsp-ai
     asm-lsp
     nixd
-    # alejandra
     nixfmt-rfc-style
     ruff
     ruff-lsp
     pyright
-    # rustup component add rust-analyzer
-    rust-analyzer
-    rustfmt
     clippy
     taplo
     nodePackages_latest.vscode-json-languageserver
@@ -242,19 +196,8 @@ in
     lldb
     cmake-language-server
     marksman
-    # markdown-oxide
     gnumake
-    texlab
-    sioyek
     yaml-language-server
-    verible
-    nodePackages.diagnostic-languageserver
-    verilator
-
-    lua-language-server
-    # vale-ls
-    # typos-lsp
-    # ltex-ls
 
     neovim
     # taskwarrior
@@ -265,26 +208,7 @@ in
     winetricks
     steam-run # quick runner for fsh compliant binaries
     udiskie
-    # (pkgs.callPackage ../../pkgs/derivations/tt.nix {})
-    # (pkgs.callPackage ../../pkgs/derivations/j4-dmenu-desktop.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/waybar_timer.nix { })
     (pkgs.callPackage ../../pkgs/derivations/xremap.nix { })
-
     (pkgs.callPackage ../../pkgs/derivations/helix.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/mitype.nix { })
-    # (pkgs.callPackage ../../pkgs/derivations/rvc-cli.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/gen_typing_test.nix { })
-    # (pkgs.callPackage ../../pkgs/derivations/notify_scheduler.nix { })
-    # (pkgs.callPackage ../../pkgs/derivations/blender.nix { })
-
-    (pkgs.callPackage ../../pkgs/derivations/veridian.nix { })
-    # (pkgs.callPackage ../../pkgs/derivations/veridian.nix { withSlang = true; })
-    (pkgs.callPackage ../../pkgs/derivations/update_wp.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/gtk_applet.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/gtk_indicator.nix { })
-    # (pkgs.callPackage ../../system/app/svlangserver.nix {})
-    (pkgs.callPackage ../../pkgs/derivations/youtube_sync.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/auto_refresh.nix { })
-    (pkgs.callPackage ../../pkgs/derivations/clipboard_tts.nix { })
   ];
 }
