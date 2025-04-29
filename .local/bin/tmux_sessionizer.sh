8a4cc2d662225ad226d7f1103f0e7a0705dcddc3
hakan-demirli <ehdemirli@gmail.com>
2025-04-29 21:04:52 +0300

    fix(tmux_sessionizer): open tmux sessions inplace

--- .local/bin/tmux_sessionizer.sh
+++ .local/bin/tmux_sessionizer.sh
@@ -1,47 +1,113 @@
 #!/usr/bin/env bash
 
-function tmux_sessionizer () {
-    selected_dir="$(find -L ~/Desktop /mnt/second/rep -maxdepth 1 \( \
-        -name node_modules -o  \
-        -name conda        -o  \
-        -name env          -o  \
-        -name bin          -o  \
-        -name .direnv      -o  \
-        -name .git         -o  \
-        -name .github      -o  \
-        -name __pycache__  -o  \
-        -name venv         -o  \
-        -name .venv            \
-    \) -prune -o -type d -print | fzf)"
+raw_input=""
 
-    if [[ -z "$selected_dir" ]]; then
-        exit 0
+if [[ $# -eq 1 ]]; then
+  raw_input=$1
+else
+  search_dirs=("$HOME/Desktop" "$HOME/Downloads")
+
+  valid_search_dirs=()
+  for dir in "${search_dirs[@]}"; do
+    if [[ -d "$dir" ]]; then
+      valid_search_dirs+=("$dir")
+    else
+      echo "Warning: Search directory '$dir' not found." >&2
     fi
-    tmux_running=$(pgrep tmux)
+  done
 
-    tmux_cwd_hash=$(echo -n "$selected_dir" | md5sum | awk '{ print $1 }')
-    session_name=$(basename "$selected_dir")_$tmux_cwd_hash
+  if [[ ${#valid_search_dirs[@]} -eq 0 ]]; then
+    echo "Error: No valid search directories found (${search_dirs[*]})." >&2
+    exit 1
+  fi
 
-    # # Log all variables to debug.log
-    # echo "selected_dir: $selected_dir" >> debug.log
-    # echo "tmux_running: $tmux_running" >> debug.log
-    # echo "tmux_cwd_hash: $tmux_cwd_hash" >> debug.log
-    # echo "session_name: $session_name" >> debug.log
+  dir_list=""
+  if command -v fd &>/dev/null; then
+    mapfile -d '' dir_array < <(fd --type d --max-depth 1 --exclude ".*" . "${valid_search_dirs[@]}" --print0)
+    dir_list=$(printf "%s\n" "${dir_array[@]}")
+  else
+    mapfile -d '' dir_array < <(find "${valid_search_dirs[@]}" -mindepth 1 -maxdepth 1 -type d ! -name ".*" -print0)
+    dir_list=$(printf "%s\n" "${dir_array[@]}")
+  fi
 
-    if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-        tmux new-session -s "$session_name" -c "$selected_dir"
-        exit 0
+  if [[ -z "$dir_list" ]]; then
+    echo "No directories found in specified search locations."
+    exit 0
+  fi
+
+  display_list=""
+  if [[ -n "$HOME" ]] && [[ "$HOME" != "/" ]]; then
+    display_list=$(echo "$dir_list" | sed "s|^${HOME}|~|")
+  else
+    display_list="$dir_list"
+  fi
+
+  selected_display=$(echo -e "$display_list" | fzf --prompt="Select project: ")
+
+  if [[ -n "$selected_display" ]]; then
+    raw_input="$selected_display"
+  else
+    raw_input=""
+  fi
+fi
+
+if [[ -z "$raw_input" ]]; then
+  echo "No directory selected."
+  exit 0
+fi
+
+path_to_resolve="$raw_input"
+
+if [[ "$path_to_resolve" == "~" ]]; then
+  if [[ -z "$HOME" ]]; then
+    echo "Error: HOME not set." >&2
+    exit 1
+  fi
+  path_to_resolve="$HOME"
+elif [[ "$path_to_resolve" == "~/"* ]]; then
+  if [[ -z "$HOME" ]]; then
+    echo "Error: HOME not set." >&2
+    exit 1
+  fi
+  path_to_resolve="$HOME/${path_to_resolve#\~/}"
+fi
+
+selected=$(realpath -e "$path_to_resolve" 2>/dev/null)
+
+if [[ $? -ne 0 || ! -d "$selected" ]]; then
+  echo "Error: Resolved path '$path_to_resolve' is not a valid directory." >&2
+  echo "(Attempted resolution: '$selected')" >&2
+  exit 1
+fi
+
+selected_name=$(basename "$selected" | tr --complement --squeeze '[:alnum:]' '_')
+
+selected_name=${selected_name#_}
+selected_name=${selected_name%_}
+
+if [[ -z "$selected_name" ]]; then
+  selected_name="default_session"
+fi
+
+if ! tmux info &>/dev/null; then
+  echo "No tmux server found. Starting new session '$selected_name'..."
+  exec tmux new-session -s "$selected_name" -c "$selected"
+else
+  echo "Tmux server found."
+  if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+    echo "Session '$selected_name' not found. Creating detached session..."
+    tmux new-session -ds "$selected_name" -c "$selected"
+    if [[ $? -ne 0 ]]; then
+      echo "Error: Failed to create tmux session '$selected_name'." >&2
+      exit 1
     fi
+  fi
 
-    if ! tmux has-session -t="$session_name" 2> /dev/null; then
-        tmux new-session -ds "$session_name" -c "$selected_dir"
-    fi
-
-    tmux switch-client -t "$session_name"
-}
-
-tmux_sessionizer "$@"
-
-
-
-
+  if [[ -n "$TMUX" ]]; then
+    echo "Already inside tmux. Switching client to session '$selected_name'..."
+    tmux switch-client -t "$selected_name"
+  else
+    echo "Outside tmux. Attaching to session '$selected_name'..."
+    exec tmux attach-session -t "$selected_name"
+  fi
+fi
