b133969dc80e8c0ff1f9de63960d2470a31455e4
hakan-demirli <ehdemirli@gmail.com>
2025-07-01 04:41:55 +0300

    chore: bump lock file

--- overlay.nix
+++ overlay.nix
@@ -26,30 +26,6 @@ _: {
         ];
       });
 
-      # https://github.com/NixOS/nixpkgs/pull/398248
-      bindfs = prev.bindfs.overrideAttrs (oldAttrs: {
-        pname = "bindfs";
-        version = "1.18.1";
-
-        src = prev.fetchurl {
-          url = "https://bindfs.org/downloads/bindfs-1.18.1.tar.gz";
-          hash = "sha256-KnBk2ZOl8lXFLXI4XvFONJwTG8RBlXZuIXNCjgbSef0=";
-        };
-
-        # This adds the reverted patch. Using `(oldAttrs.patches or []) ++` is a
-        # safe way to add patches, even if the original package had none.
-        patches = (oldAttrs.patches or [ ]) ++ [
-          (prev.fetchpatch {
-            url = "https://github.com/mpartel/bindfs/commit/3293dc98e37eed0fb0cbfcbd40434d3c37c69480.patch";
-            hash = "sha256-dtjvSJTS81R+sksl7n1QiyssseMQXPlm+LJYZ8/CESQ=";
-            revert = true;
-          })
-        ];
-
-        # The diff adds autoreconfHook, so we append it to the existing nativeBuildInputs.
-        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.autoreconfHook ];
-      });
-
     })
   ];
 }
