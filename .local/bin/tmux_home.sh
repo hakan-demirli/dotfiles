c884c09bc223e344c8d1ee602f69dfa562ff3dfe
hakan-demirli <ehdemirli@gmail.com>
2025-07-19 04:06:52 +0300

    feat: add tmux_home and use it by default

---
+++ .local/bin/tmux_home.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+TARGET_DIR="$HOME"
+
+SESSION_NAME="home_$(echo -n "$TARGET_DIR" | md5sum | cut -d' ' -f1)"
+
+if [ -n "$TMUX" ]; then
+    if ! tmux has-session -t="$SESSION_NAME" 2>/dev/null; then
+        tmux new-session -ds "$SESSION_NAME" -c "$TARGET_DIR"
+    fi
+    tmux switch-client -t "$SESSION_NAME"
+else
+    if ! tmux info &>/dev/null; then
+        tmux new-session -ds "$SESSION_NAME" -c "$TARGET_DIR"
+    else
+        if ! tmux has-session -t="$SESSION_NAME" 2>/dev/null; then
+            tmux new-session -ds "$SESSION_NAME" -c "$TARGET_DIR"
+        fi
+    fi
+    exec tmux attach-session -t "$SESSION_NAME"
+fi
