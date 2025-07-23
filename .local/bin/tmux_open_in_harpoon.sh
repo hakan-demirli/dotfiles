e8ffa5b87c2f700ae767eb6a52985d5f2d106825
hakan-demirli <ehdemirli@gmail.com>
2025-07-23 05:06:27 +0300

    feat(tmux_harpoon): add open selected path

---
+++ .local/bin/tmux_open_in_harpoon.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# open file path in first hx
+
+OPEN_IN_SPLIT=0
+if [[ "${1:-}" == "-s" ]]; then
+  OPEN_IN_SPLIT=1
+fi
+
+input_path=$(xargs) # trim whitespace
+[ -z "$input_path" ] && exit 0 # Exit if no path was selected
+
+pane_path=$(tmux display-message -p '#{pane_current_path}')
+
+cd "$pane_path"
+absolute_path=$(realpath -m -- "$input_path")
+# The '-m' flag allows the path to not exist, so we can open new files.
+
+tmux_cwd_hash=$(echo -n "$pane_path" | md5sum | awk '{ print $1 }')
+cache_dir="$HOME/.cache/tmux_harpoon"
+data_file="$cache_dir/$tmux_cwd_hash.csv"
+
+if [[ ! -f "$data_file" ]]; then
+  tmux display-message "Harpoon file not found for this directory."
+  exit 1
+fi
+
+target_session=""
+target_window=""
+found_hook=0
+
+while IFS= read -r line; do
+  [[ "$line" =~ ^# || -z "$line" ]] && continue
+
+  tmux_command_target=$(echo "$line" | cut -d, -f2)
+
+  if [[ "$tmux_command_target" == "hx" ]]; then
+    target_window=$(echo "$line" | cut -d, -f1)
+    found_hook=1
+    break
+  fi
+done < "$data_file"
+
+if [[ $found_hook -eq 0 ]]; then
+  tmux display-message "No 'hx' harpoon hook found."
+  # tmux send-keys "hx '${absolute_path}'" Enter
+  exit 1
+fi
+
+session_line=$(grep '^# session_name:' "$data_file" || true)
+target_session="${session_line#*: }"
+
+if [[ -z "$target_session" ]]; then
+    tmux display-message "Could not determine harpoon session name."
+    exit 1
+fi
+
+tmux select-window -t "$target_session:$target_window"
+
+helix_command=":open '${absolute_path}'"
+
+if [[ $OPEN_IN_SPLIT -eq 1 ]]; then
+  tmux send-keys -t "$target_session:$target_window" ":split" Enter
+  tmux send-keys -t "$target_session:$target_window" "$helix_command" Enter
+else
+  tmux send-keys -t "$target_session:$target_window" "$helix_command" Enter
+fi
