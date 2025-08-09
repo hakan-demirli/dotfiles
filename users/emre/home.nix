4077b648c1ab4fb4e472f0fff4aca0b5e378fa7b
hakan-demirli <ehdemirli@gmail.com>
2025-08-10 01:15:40 +0300

    fix(theme): add gnome color-scheme

--- users/emre/home.nix
+++ users/emre/home.nix
@@ -38,6 +38,8 @@ in
 
   # dconf dump / | dconf2nix > dconf.nix
   dconf.settings = {
+    "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
     "org/virt-manager/virt-manager/connections" = {
       autoconnect = [ "qemu:///system" ];
       uris = [ "qemu:///system" ];
