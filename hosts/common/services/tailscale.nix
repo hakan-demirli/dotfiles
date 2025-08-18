4907eadf513b8988a34bbb486f4022d015ec6334
hakan-demirli <ehdemirli@gmail.com>
2025-08-18 20:31:27 +0300

    fix(tailscale): use resolved for dns resolution

--- hosts/common/services/tailscale.nix
+++ hosts/common/services/tailscale.nix
@@ -1,15 +1,30 @@
 {
+  config,
   reverseSshRemoteHost ? throw "You must specify a reverseSshRemoteHost",
   ...
 }:
 {
+  # you must disconnect warp
+  # warp-cli disconnect
   services.tailscale = {
     enable = true;
     authKeyFile = "/persist/home/emre/Desktop/dotfiles/secrets/tailscale-key";
+    useRoutingFeatures = "client";
     extraUpFlags = [
       "--login-server=https://${reverseSshRemoteHost}"
     ];
   };
+
+  networking.firewall = {
+    checkReversePath = "loose";
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+  };
+
+  networking.networkmanager.unmanaged = [ "tailscale0" ];
+
+  services.resolved.enable = true;
+  networking.networkmanager.dns = "systemd-resolved";
 
   environment.persistence."/persist/system".directories = [
     "/var/lib/tailscale"
