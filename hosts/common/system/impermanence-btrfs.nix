ea719c67f62c9480f92dca3b928db165b0b96d37
hakan-demirli <ehdemirli@proton.me>
2025-12-15 05:47:47 +0300

    chore(impermanence): reduce old root retention period

--- hosts/common/system/impermanence-btrfs.nix
+++ hosts/common/system/impermanence-btrfs.nix
@@ -23,7 +23,7 @@
         btrfs subvolume delete "$1"
     }
 
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +15); do
         delete_subvolume_recursively "$i"
     done
 
