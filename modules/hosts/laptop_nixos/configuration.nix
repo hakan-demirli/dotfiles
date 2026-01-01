4ffa1c35ae1137b6f8bfcc83dcae1890c0cd19cd
hakan-demirli <ehdemirli@proton.me>
2026-01-01 11:24:20 +0100

    fix(persistence): persist .config/Antigravity

--- modules/hosts/laptop_nixos/configuration.nix
+++ modules/hosts/laptop_nixos/configuration.nix
@@ -72,6 +72,7 @@ in
             ".mozilla"
             ".local/share"
             ".antigravity"
+            ".config/Antigravity"
             ".gemini"
             "Desktop"
             "Documents"
