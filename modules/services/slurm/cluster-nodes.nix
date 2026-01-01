0dedfb5bad64c2a9368cd9730c712f15d40ac5ea
hakan-demirli <ehdemirli@proton.me>
2026-01-01 19:42:57 +0100

    fix(ci): address lint warnings

--- modules/services/slurm/cluster-nodes.nix
+++ modules/services/slurm/cluster-nodes.nix
@@ -1,26 +1,24 @@
 _: {
-  flake.modules.nixos.slurm-cluster-nodes =
-    { ... }:
-    {
-      services.slurm-cluster = {
-        masterHostname = "vm-oracle-aarch64";
-        clusterNodes = [
-          {
-            hostName = "vm-oracle-aarch64";
-            cores = 4;
-            ramMb = 21000;
-          }
-          {
-            hostName = "s01";
-            cores = 16;
-            ramMb = 77000;
-          }
-          {
-            hostName = "s02";
-            cores = 8;
-            ramMb = 14500;
-          }
-        ];
-      };
+  flake.modules.nixos.slurm-cluster-nodes = _: {
+    services.slurm-cluster = {
+      masterHostname = "vm-oracle-aarch64";
+      clusterNodes = [
+        {
+          hostName = "vm-oracle-aarch64";
+          cores = 4;
+          ramMb = 21000;
+        }
+        {
+          hostName = "s01";
+          cores = 16;
+          ramMb = 77000;
+        }
+        {
+          hostName = "s02";
+          cores = 8;
+          ramMb = 14500;
+        }
+      ];
     };
+  };
 }
