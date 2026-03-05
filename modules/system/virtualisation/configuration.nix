0edc5579ba2926620c46ae9499a58743e05ad4aa
hakan-demirli <78746991+hakan-demirli@users.noreply.github.com>
2026-03-05 15:28:23 +0100

    chore: bump lockfile (#67)

--- modules/system/virtualisation/configuration.nix
+++ modules/system/virtualisation/configuration.nix
@@ -14,6 +14,16 @@
         '';
       };
 
+      # Fix https://github.com/NixOS/nixpkgs/issues/496836
+      systemd.services.virt-secret-init-encryption = {
+        serviceConfig = {
+          ExecStart = [
+            "" # Clear the existing ExecStart
+            "${pkgs.bash}/bin/sh -c 'umask 0077 && (${pkgs.coreutils}/bin/dd if=/dev/random status=none bs=32 count=1 | ${pkgs.systemd}/bin/systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
+          ];
+        };
+      };
+
       environment.systemPackages = with pkgs; [
         virtiofsd
         virt-viewer
