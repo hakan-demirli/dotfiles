c06e6ab17613fc8c3de44cd09bb39703a042033a
hakan-demirli <78746991+hakan-demirli@users.noreply.github.com>
2026-04-14 22:20:30 +0300

    fix(earlyoom): dont kill pipewire and wireplumber (#88)

--- modules/services/earlyoom/configuration.nix
+++ modules/services/earlyoom/configuration.nix
@@ -10,7 +10,7 @@
       extraArgs = [
         "-g"
         "--avoid"
-        "^(kitty|ssh|sshd|systemd|systemd-logind|sddm|Hyprland|Xorg|waybar|scrd|dbus-daemon|gpg-agent|ssh-agent)$"
+        "^(kitty|ssh|sshd|systemd|systemd-logind|sddm|Hyprland|Xorg|waybar|pipewire-pulse|wireplumber|scrd|dbus-daemon|gpg-agent|ssh-agent)$"
         "--prefer"
         "^(electron|chrom|java|node|nix-daemon|cc1plus|rustc|cargo|gcc)$"
       ];
