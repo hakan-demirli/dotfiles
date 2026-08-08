ef4ae71e825574495cb4408e6d4ab3e6407a2a87
hakan-demirli <ehdemirli@proton.me>
2026-08-08 22:53:33 +0200

    fix: dry headscale acl config

--- modules/nix/checks/lib/default.nix
+++ modules/nix/checks/lib/default.nix
@@ -8,7 +8,10 @@
 {
   bootstrap-authentication = import ./bootstrap-authentication.nix { inherit pkgs self inputs; };
   bootstrap-secrets = import ./bootstrap-secrets.nix { inherit pkgs self lib; };
-  codegen-smoke = import ./codegen-smoke.nix { inherit pkgs self; };
+  codegen-smoke = import ./codegen-smoke.nix { inherit pkgs self inputs; };
+  generated-headscale-policy = import ./generated-headscale-policy.nix {
+    inherit pkgs self inputs;
+  };
   file-transfer = import ./file-transfer.nix { inherit pkgs self lib; };
   intent = import ./intent.nix { inherit pkgs self; };
   inventory-eval = import ./inventory-eval.nix { inherit pkgs self lib; };
