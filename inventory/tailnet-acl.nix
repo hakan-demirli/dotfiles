38f4145ed2cbe1efcdfa0d6203a66de948ec0743
hakan-demirli <ehdemirli@proton.me>
2026-09-15 04:15:36 +0200

    fix(ntfy): acl permit ntfy

--- inventory/tailnet-acl.nix
+++ inventory/tailnet-acl.nix
@@ -4,4 +4,9 @@
     src = [ "tag:cluster-router-0" ];
     dst = [ "tag:cluster-user-0-fleet-controller:5514" ];
   }
+  {
+    action = "accept";
+    src = [ "tag:cluster-user-0-fleet" ];
+    dst = [ "tag:cluster-user-0-fleet-controller:8111" ];
+  }
 ]
